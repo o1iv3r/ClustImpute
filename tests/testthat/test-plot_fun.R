7323abe6b234268189c456caf490a811c5aa0e9b
Oliver Pfaffel <opfaffel@gmail.com>
2021-03-13 19:35:31 +0100

    re-added test

--- tests/testthat/test-plot_fun.R
+++ tests/testthat/test-plot_fun.R
@@ -4,6 +4,7 @@ frac_missing <- .1
 nr_cluster <- 1
 
 # Random data
+set.seed(101)
 X <- as.data.frame(matrix(rnorm(2*n),n,2))
 Xmiss <- miss_sim(X,p=frac_missing,type="MCAR")
 
@@ -20,6 +21,11 @@ test_that("Plot returns a ggplot2 object", {
   expect_equal(class(box_plot)[2], "ggplot")
 })
 
+test_that("Plot throws error for wrong imputs", {
+  expect_error(plot(res,type="centoids2"), "type must bei either 'hist' or 'box'")
+  expect_error(plot(res,vline="mean2"), "vline must be either 'centroids' or 'mean'")
+})
+
 test_that("Print returns an output to the console", {
-  expect_equal(output_console[5],"| -0.0277586| -0.0267396|       1|")
+  expect_equal(output_console[5],"| -0.0196439| -0.0667993|       1|")
 })
